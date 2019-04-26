0491a5680897e120aba8759e08c24299145f18c9
Baysan <46582456+mebaysan@users.noreply.github.com>
2019-04-27 00:58:50 +0300

    Notes

---
+++ Attributes/Attributes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{ // Attribute'lar nesneye anlam vermemizi sağlar
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id = 1, FirstName = "Enes", LastName = "Baysan" };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+        }
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty] //-> attribute koyduk(aşağıda oluşturduk)
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        [RequiredProperty] // 2 kere kullandık, AllowMultiple sayesinde
+        public string LastName { get; set; }
+    }
+
+    class CustomerDal
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0} {1} {2} added! ", customer.Id, customer.FirstName, customer.LastName);
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field,AllowMultiple =true)] // -> Bu attribute propertylere eklenebilir dedik .Class deseydik sadece class'larda kullanabilirdik. '|' diyerek diyer Targetsleri set edebiliriz. AllowMultiple -> birden fazla kullanmamızı sağlar(true ise kullanlır, false ise kullanılmaz)
+    class RequiredPropertyAttribute : Attribute // attribute olduğunu ifade ediyoruz.
+    {
+        
+    }
+}
---
+++ Reflections/Reflections/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem(2, 3);
+            Console.WriteLine(dortIslem.Topla(5, 6)); // -> parametre olarak aldığı değerler ile çalışıyor
+            Console.WriteLine(dortIslem.Topla2()); // -> constructor daki değerler ile çalışıyor
+
+            var tip = typeof(DortIslem); // -> benim çalışacağım bir tip var ve bu DortIslem'dir dedik.
+
+            DortIslem dortIslem2 = (DortIslem) Activator.CreateInstance(tip,8,9); // -> 8,9 diyerek constructor parametrelerini verdik
+            //tip.GetType().GetMethod("Topla2").Invoke(tip,null); // -> GetMethod ile istediğimiz methoda ulaşırız, Invoke diyerek o methodu çalıştırırız.
+
+            var metodlar = tip.GetMethods(); // methodları metodlar diye bir listede topladık
+            foreach (var item in metodlar)
+            {
+                Console.WriteLine("Metod Adı : {0}", item.Name);
+                foreach ( var parametre in item.GetParameters()) // her item nesnesinin Parametresi için
+                {
+                    Console.WriteLine("Parametre = {0}", parametre.Name);
+                }
+                foreach (var attribute in item.GetCustomAttributes())
+                {
+                    Console.WriteLine("Attribute Name : {0}", attribute.GetType().Name);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+    public class DortIslem
+    {
+        private int _sayi1;
+        private int _sayi2;
+        public DortIslem(int sayi1,int sayi2)
+        {
+            _sayi1 = sayi1;
+            _sayi2 = sayi2;
+
+
+        }
+        public int Topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+        public int Topla2()
+        {
+            return _sayi1 + _sayi2;
+        }
+
+        public int Carp2()
+        {
+            return _sayi1 * _sayi2;
+        }
+
+    }
+}
