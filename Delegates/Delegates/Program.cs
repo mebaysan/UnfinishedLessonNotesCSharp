796c3c20a878a80e7f4970c4e9ba7bed8acb5bd8
Baysan <46582456+mebaysan@users.noreply.github.com>
2019-04-28 13:36:37 +0300

    Add files via upload

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{   // Delegate -> bir nevi elçi diyebiliriz.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            MyDelegate myDelegate = customerManager.SendMessage; // -> myDelegate elçisi customerManager'in SendMessage fonksiyonuna elçilik yapar
+            myDelegate += customerManager.ShowAlert; // -> bu delegeye diğer operasyonu da atadık
+            myDelegate(); // -> delegeyi çağırdık ve işlem gerçekleşti
+            myDelegate -= customerManager.SendMessage; // -> aynı şekilde bu delegeden operasyonu silebiliriz.
+            MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+            myDelegate2("enes"); // -> parametreli delege
+            Func<int, int, int> add = Topla; // Func tipinde bir delege oluşturduk, <parametre tipi , parametre tipi , değer dönüş tipi>
+            Console.WriteLine(add(3, 5));
+            Func<int> getRandomNumber = delegate () // parametresiz func olduğu için sadece return değer dönüş tipini vermemiz yeterli
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+            Console.WriteLine(getRandomNumber);// parametresiz method kullandık
+            Console.ReadLine();
+        }
+        // FUNC İLE ÇALIŞMAK
+        static int Topla(int x, int y) // Func delegeye atayacağımız method static olmalı!
+        {
+            return x + y;
+        }
+    }
+   
+
+    public delegate void MyDelegate(); // -> parametre almayan, return döndürmeyen ve void olan operasyonlara delegate deriz.
+    public delegate void MyDelegate2(string kelime);
+    public class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Hello!");
+        }
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be careful!");
+        }
+        public void SendMessage2(string kelime)
+        {
+            Console.WriteLine("Hello!" + kelime);
+        }
+    }
+
+}
---
+++ Events/Events/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product laptop = new Product(50);
+            laptop.ProductName = "Laptop";
+            Product gsm = new Product(50);
+            gsm.ProductName = "GSM";
+            gsm.StockControlEvent += Gsm_StockControlEvent; // event'e kayıt olduk!
+            for (int i = 0; i < 10; i++)
+            {
+                laptop.Sell(10);
+                gsm.Sell(10);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Gsm_StockControlEvent()
+        {
+            Console.WriteLine("Gsm about to finish!");
+        }
+    }
+    public delegate void StockControl();
+    public class Product
+    {
+        private int _stock;
+
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+
+        public event StockControl StockControlEvent; // event oluşturduk
+        public string ProductName { get; set; }
+        public int Stock {
+            get { return _stock; }
+            set {
+                _stock = value;
+                if (value<=15 && StockControlEvent!=null)
+                {
+                    StockControlEvent();
+                }
+            }
+        }
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine("{0} Stock Amount = {1}",ProductName, Stock);
+        }
+    }
+}
